aa11cc307a607b5a879b48043e9eb5f20710cc8a
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-15 19:24:10 +0530

    Update N-Triangles.java

--- N-Triangles.java
+++ N-Triangles.java
@@ -1,1 +1,13 @@
+//link = https://www.codingninjas.com/codestudio/problems/n-triangles_6573689?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for (int i = 1; i <=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+        System.out.println();
+        }
+    }
+}
