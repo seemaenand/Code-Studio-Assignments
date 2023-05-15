21202d4ae436309ae94dd94d3369888afa8e2c3f
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-15 19:48:53 +0530

    Update Reverse Number Triangle.java

--- Reverse Number Triangle.java
+++ Reverse Number Triangle.java
@@ -1,1 +1,14 @@
+// link - https://www.codingninjas.com/codestudio/problems/reverse-number-triangle_6581889?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        // Write your code here
+        for (int i = 1; i<=n; i++){
+            for (int j=1; j<=n-i+1; j++){
+                System.out.print(j + " ");
+            }
+        System.out.println();
+        System.out.println();
+        }
+    }
+}
