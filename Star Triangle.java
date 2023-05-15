df2ff8a522929c1694d20d8a63c4d5c316465938
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-15 23:09:10 +0530

    Update Star Triangle.java

--- Star Triangle.java
+++ Star Triangle.java
@@ -1,1 +1,20 @@
+//link - https://www.codingninjas.com/codestudio/problems/star-triangle_6573671?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=1
 
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+
+        for(int i=0; i<n; i++){
+            //spaces
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int z=0; z<(2*i)+1; z++){
+                System.out.print("*");
+            }
+        System.out.println();
+        System.out.println();
+        }
+    }
+}
