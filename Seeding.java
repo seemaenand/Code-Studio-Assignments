dc7f24f84f174d7ba5b7a7f4ec2005a54e42e95a
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-15 19:43:12 +0530

    Update Seeding.java

--- Seeding.java
+++ Seeding.java
@@ -1,1 +1,15 @@
+// link - https://www.codingninjas.com/codestudio/problems/seeding_6581892?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+
+public class Solution {
+    public static void seeding(int n) {
+        // Write your code here
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n-i; j++){
+                System.out.print("*" + " ");
+            }
+        System.out.println();
+        System.out.println();
+        }
+    }
+}
