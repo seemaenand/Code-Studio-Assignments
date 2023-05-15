384c5036908bf9a756b97f0008c1872653e4232c
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-15 19:36:59 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,1 +1,14 @@
+// link - https://www.codingninjas.com/codestudio/problems/triangle_6573690?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+        System.out.println();
+        System.out.println();
+        }
+    }
+}
