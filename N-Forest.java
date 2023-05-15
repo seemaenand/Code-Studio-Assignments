e990adbe2629a60dd96740d69a522f97bf3db31f
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-15 19:02:41 +0530

    Update N-Forest.java

--- N-Forest.java
+++ N-Forest.java
@@ -1,1 +1,15 @@
-link
+// link for the question - https://www.codingninjas.com/codestudio/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
+
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+
+        for(int i = 0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*"+" ");
+            }
+        System.out.println();
+        System.out.println();
+        }
+    }
+}
