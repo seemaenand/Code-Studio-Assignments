b8d1caab9b5f5b42a07d7f736fb35ae5e12c7a8f
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 20:51:14 +0530

    Update Rotated Triangle.java

--- Rotated Triangle.java
+++ Rotated Triangle.java
@@ -1,1 +1,19 @@
+//link - https://www.codingninjas.com/codestudio/problems/rotated-triangle_6573688?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+
+      
+        for(int i = 1; i<=2*n - 1; i++){
+           int stars = i;
+           if(i>n){
+               stars = 2*n-i;
+           }
+           for(int j = 1; j<=stars; j++ ){
+               System.out.print("*");
+           }
+           System.out.println();
+        }
+    }
+}
