b35103f00dea8078c0066a12dad3af4747cdf784
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 21:25:33 +0530

    Update Increasing Number Triangle.java

--- Increasing Number Triangle.java
+++ Increasing Number Triangle.java
@@ -1,1 +1,17 @@
+//link - https://www.codingninjas.com/codestudio/problems/increasing-number-triangle_6581893?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
 
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        // Write your code here
+
+        int count = 1;
+
+        for (int i = 1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
