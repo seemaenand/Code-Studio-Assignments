f730c193feeed4acb628c54c5c84330df20d5ca2
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 22:27:35 +0530

    Update Increasing Letter Triangle.java

--- Increasing Letter Triangle.java
+++ Increasing Letter Triangle.java
@@ -1,1 +1,14 @@
+//link - https://www.codingninjas.com/codestudio/problems/increasing-letter-triangle_6581897?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+        for(int i= 0; i<n;  i++){
+            for(char ch='A'; ch <='A' + i; ch++){
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 
