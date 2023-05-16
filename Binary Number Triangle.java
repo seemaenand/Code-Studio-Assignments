bd2f77711ad7ceaf54e25aa15b399bbe8527971b
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 21:06:24 +0530

    Update Binary Number Triangle.java

--- Binary Number Triangle.java
+++ Binary Number Triangle.java
@@ -1,1 +1,21 @@
+//link - https://www.codingninjas.com/codestudio/problems/binary-number-triangle_6581890?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void nBinaryTriangle(int n) {
+        // Write your code here.
+        int start = 1;
+        for (int i=1; i<=n; i++){
+            if(i%2 == 0){
+                start = 0;
+            } else {
+                start = 1;
+            }
+            for (int j = 1; j<=i; j++){
+                System.out.print(start+" ");
+                start = 1 - start; 
+            }
+            System.out.println();
+        }
+
+    }
+}
