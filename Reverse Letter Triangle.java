2052893860f6ac51acdf7da0f87a55dd7ea24948
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 22:32:36 +0530

    Update Reverse Letter Triangle.java

--- Reverse Letter Triangle.java
+++ Reverse Letter Triangle.java
@@ -1,1 +1,18 @@
+//link - https://www.codingninjas.com/codestudio/problems/reverse-letter-triangle_6581906?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
+
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        // Write your code here
+
+         for(int i= 1; i<=n;  i++){
+            for(char ch='A'; ch <='A' + n - i; ch++){
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+    
 
