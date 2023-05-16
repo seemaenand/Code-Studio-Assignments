cb7d53ac2e667abbecb9caeff09cd0b02d376b68
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 22:37:01 +0530

    Update Alpha-Ramp.java

--- Alpha-Ramp.java
+++ Alpha-Ramp.java
@@ -1,1 +1,20 @@
+//link - https://www.codingninjas.com/codestudio/problems/alpha-ramp_6581888?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
+
+public class Solution {
+    public static void alphaRamp(int n) {
+        // Write your code here
+
+        char ch = 'A';
+        for(int i= 1; i<=n;  i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(ch + " ");
+                
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+}
+
+    
 
