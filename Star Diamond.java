7bddd6cafd15f85097c0a362ea81b9f6fa54b6f3
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-16 20:07:06 +0530

    Update Star Diamond.java

--- Star Diamond.java
+++ Star Diamond.java
@@ -1,1 +1,34 @@
+//link - https://www.codingninjas.com/codestudio/problems/star-diamond_6573686?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void nStarDiamond(int n) {
+        // Write your code here
+        
+        //top half
+
+        for(int i = 0; i<n; i++){
+            //spaces
+            for(int j= 0; j< n-i-1; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j = 0; j<(2*i)+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //bottom half
+        for(int l = 0; l<n; l++){
+            //spaces
+            for(int j = 0; j<l; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=0; j< (2*n-2*l)-1 ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
