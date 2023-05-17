5e7f8c614c867e5ec864aa8e15d91db029a44da7
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-17 09:42:23 +0530

    Update Alpha Hill.java

--- Alpha Hill.java
+++ Alpha Hill.java
@@ -1,1 +1,30 @@
+//link - https://www.codingninjas.com/codestudio/problems/alpha-hill_6581921?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems&leftPanelTab=0
 
+public class Solution {
+    public static void alphaHill(int n) {
+        // Write your code here
+
+        for (int i=0; i<n; i++){
+            //spaces
+            for (int j=0; j<n-i-1; j++){
+                System.out.print("  ");
+            }
+
+            //left alpha
+            char ch = 'A';
+            int breakpoint = ((2*i)+1)/2;
+            for(int j = 1; j<= 2*i+1; j++){
+                System.out.print(ch + " ");
+                if (j<=breakpoint){
+                ch++;
+                } else{
+                ch--;
+                }
+            }
+
+            
+            System.out.println();
+        
+        }
+    }
+}
