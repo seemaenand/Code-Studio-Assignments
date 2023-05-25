42dfd07c943478cd2db793ae25999546efb8f15c
Seema Enand <128905245+seemaenand@users.noreply.github.com>
2023-05-25 12:50:21 +0530

    Update Alpha-Triangle.java

--- Alpha-Triangle.java
+++ Alpha-Triangle.java
@@ -1,3 +1,16 @@
 //link - https://www.codingninjas.com/codestudio/problems/alpha-triangle_6581429?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
 
+public class Solution {
+    public static void alphaTriangle(int n) {
+        // Write your code here
 
+        char startAlpha = (char)(int)('A' + n - 1);
+
+        for(int i=0; i<n; i++){
+            for (char j = startAlpha; j>=(char)(int)(startAlpha - i); j--){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
